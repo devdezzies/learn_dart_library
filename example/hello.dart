4030be1827e19ed0614a9e98195866d4aeda70ab
Abdullah <boltholt17@gmail.com>
2024-02-10 14:58:07 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,11 @@
+import 'package:learn_dart_library/hello.dart';
+
+void main(){
+  print(sayHello("Jonathan"));
+
+  var customer = Customer();
+  var product = Product();
+  var category = Category();
+  
+  // to run this project type dart example/hello.dart
+}
---
+++ example/learn_dart_library_example.dart
@@ -0,0 +1,6 @@
+import 'package:learn_dart_library/learn_dart_library.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
